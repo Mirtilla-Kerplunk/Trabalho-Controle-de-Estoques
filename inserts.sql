6f34e777ea645f4e74fe16632ca2fd87dfa304f9
Lua <melcherpradoalves@gmail.com>
2026-06-01 23:32:28 -0400

    inserts

---
+++ inserts.sql
@@ -0,0 +1,11 @@
+INSERT INTO Categoria(nome, descricao)
+VALUES
+('Eletrônicos', 'Produtos eletrônicos');
+
+INSERT INTO Fornecedor(nome)
+VALUES
+('Razer');
+
+INSERT INTO Produto(nome, preco, categoria_id, fornecedor_id)
+VALUES
+('Mouse Gamer', 99.90, 1, 1);
